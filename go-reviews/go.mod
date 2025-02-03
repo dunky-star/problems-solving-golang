328632b5939487ae99c4c25d33505407ea5c5ab1
dunky-star <dunkygeoffrey39@gmail.com>
2025-02-03 12:35:01 -0700

    Explicit type conversion in Go

---
+++ go-reviews/go.mod
@@ -0,0 +1,3 @@
+module reviews
+
+go 1.23.5
---
+++ go-reviews/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// String
+	var a string = "Foo"
+	fmt.Println("String data: ", a)
+
+	// Inttegers
+	var b int = 99
+	fmt.Println("Ineteger data: ", b)
+
+	// Float type
+	d := 3.1415 // Flaot64
+	var e int = int(d)
+	fmt.Println("Type conversion: ", e)
+
+}
