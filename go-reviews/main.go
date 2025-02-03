280235154afcdbad05dfec70f04c130c53f736ce
dunky-star <dunkygeoffrey39@gmail.com>
2025-02-03 12:40:47 -0700

    Explicit type conversion in Go

--- go-reviews/main.go
+++ go-reviews/main.go
@@ -4,16 +4,21 @@ import "fmt"
 
 func main() {
 	// String
-	var a string = "Foo"
-	fmt.Println("String data: ", a)
+	var s string = "Foo"
+	fmt.Println("String data: ", s)
 
 	// Inttegers
-	var b int = 99
-	fmt.Println("Ineteger data: ", b)
+	var num1 int = 99
+	fmt.Println("Ineteger data: ", num1)
 
 	// Float type
 	d := 3.1415 // Flaot64
 	var e int = int(d)
 	fmt.Println("Type conversion: ", e)
+
+	// Multiple variabe declaration in 1 line
+	a, b := 10, 5 // Go allows multiple variables to be initialized at once
+	c := a + b    // 15 - addition
+	fmt.Println("Value c: ", c)
 
 }
