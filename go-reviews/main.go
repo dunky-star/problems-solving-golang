074b05869bc481bf8c75c96ed109ad270bc680a3
dunky-star <dunkygeoffrey39@gmail.com>
2025-02-03 14:18:42 -0700

    Pointer concepts in Golang

--- go-reviews/main.go
+++ go-reviews/main.go
@@ -9,6 +9,7 @@ func main() {
 		const2      = true
 		constPi     = 3.1415 // Flaot64
 		constCopied          // Unassigned constant receive previous value
+		constIota   = iota
 	)
 
 	// String
@@ -20,12 +21,20 @@ func main() {
 	fmt.Println("Integer data: ", num1)
 
 	// Float type
-	d := 200.233
-	var e int = int(d)
+	f64 := 200.233
+	var e int = int(f64)
 	fmt.Println("Type conversion: ", e)
 
 	// Multiple variabe declaration in 1 line
-	a, b := 10, 4                 // Go allows multiple variables to be initialized at once
-	fmt.Println("Value c: ", a+b) // addition = 15
-	fmt.Println("Modulus a(mod)b", a%const1)
+	x, y := 10, 4                 // Go allows multiple variables to be initialized at once
+	fmt.Println("Value c: ", x+y) // addition = 15
+	fmt.Println("Modulus a(mod)b", x%const1)
+	fmt.Println("IOTA: ", constIota)
+
+	// Pointers
+	a := 100
+	b := &a //(b points to a)
+	c := *b // Derefence to get value stored
+	fmt.Println("[b] points to [a]", b)
+	fmt.Println("Dereferencing to extract value from b: ", c)
 }
