b951f561f69226dc26109fa3841f4b8a2f3527c2
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2025-02-14 15:21:37 -0800

    feat(Golang): Basic to advanced Concepts

--- go-reviews/functions-go.go
+++ go-reviews/functions-go.go
@@ -10,10 +10,45 @@ func main() {
 	fmt.Println(strings.Repeat("_", 10))
 	greet("\nGeoffrey", "Hills", "Kitty\n")
 
+	name, otherName := "Geoffrey", "Other name Opiyo"
+	fmt.Println("Name is: ", name)
+	fmt.Println("Other name is: ", otherName)
+	myFunc(name, &otherName) // Dereferencing
+	fmt.Println(name)
+	fmt.Println(otherName)
+
+	fmt.Println("The result of the addition operation is: ", add(10, 30))
+	result, success := divide(5, 0)
+	if success {
+		fmt.Println("Division result: ", result)
+	} else {
+		fmt.Println("Error: divide by zero")
+	}
+
 }
 
 func greet(names ...string) { // Variadic parameters
 	for _, n := range names {
 		fmt.Println(n)
 	}
+}
+
+// Passing values and pointers
+// NB: Use pointers only when you want to share memory otherwise use values
+func myFunc(name string, otherName *string) {
+	name = "New name"
+	*otherName = "Other new name"
+}
+
+// Returning a single value
+func add(a, b int) int {
+	return a * b
+}
+
+// Returning multiple values
+func divide(l, r int) (int, bool) {
+	if r == 0 {
+		return 0, false
+	}
+	return l / r, true
 }
