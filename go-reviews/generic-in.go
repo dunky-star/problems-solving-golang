775186719614c9fca7ae6283f80afaa44032b6ee
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2025-02-14 21:33:36 -0800

    feat(Golang): Generic in Go

--- go-reviews/generic-in.go
+++ go-reviews/generic-in.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	a1 := []int{10, 20, 30}
+	// a2 := []float64{3.14, 11.7, 4.4}
+	// a3 := []string{"Geo", "Dun", "Karl"}
+
+	s1 := addVal(a1)
+
+	fmt.Printf("Sum of %v: %v\n", a1, s1)
+
 	testScores := []float32{
 		97.7,
 		100.0,
@@ -11,15 +19,41 @@ func main() {
 		30,
 	}
 
-	c := clone(testScores)
+	testScoresMap := map[string]float64{
+		"Geoffrey": 97.7,
+		"Amit":     98,
+		"Macs":     100,
+		"Ronald":   97,
+		"Neville":  99,
+	}
 
-	fmt.Println(&testScores[0], &c[0], c)
+	cScores := cloneSlice(testScores)
+	cMap := cloneMap(testScoresMap)
+
+	fmt.Println(&testScores[0], &cScores[0], cScores)
+	fmt.Println(cMap)
 }
 
-func clone[V any](s []V) []V {
+func cloneSlice[V any](s []V) []V {
 	result := make([]V, len(s))
 	for i, v := range s {
 		result[i] = v
+	}
+	return result
+}
+
+func cloneMap[K comparable, V any](m map[K]V) map[K]V {
+	result := make(map[K]V, len(m))
+	for k, v := range m {
+		result[k] = v
+	}
+	return result
+}
+
+func addVal(s []int) int {
+	var result int
+	for _, v := range s {
+		result += v
 	}
 	return result
 }
