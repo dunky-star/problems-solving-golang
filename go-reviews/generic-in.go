74e0157e89cbb31d2bc5d8c75a03876573829e03
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2025-02-14 22:32:16 -0800

    feat(Golang): Generic in Go

--- go-reviews/generic-in.go
+++ go-reviews/generic-in.go
@@ -4,13 +4,18 @@ import "fmt"
 
 func main() {
 	a1 := []int{10, 20, 30}
-	// a2 := []float64{3.14, 11.7, 4.4}
-	// a3 := []string{"Geo", "Dun", "Karl"}
+	a2 := []float64{3.14, 11.7, 4.4}
+	a3 := []string{"Geo", "Dun", "Karl"}
 
 	s1 := addVal(a1)
+	s2 := addVal(a2)
+	s3 := addVal(a3)
 
 	fmt.Printf("Sum of %v: %v\n", a1, s1)
+	fmt.Printf("Sum of %v: %v\n", a2, s2)
+	fmt.Printf("Sum of %v: %v\n", a3, s3)
 
+	// Slice
 	testScores := []float32{
 		97.7,
 		100.0,
@@ -19,6 +24,7 @@ func main() {
 		30,
 	}
 
+	// Map
 	testScoresMap := map[string]float64{
 		"Geoffrey": 97.7,
 		"Amit":     98,
@@ -50,8 +56,13 @@ func cloneMap[K comparable, V any](m map[K]V) map[K]V {
 	return result
 }
 
-func addVal(s []int) int {
-	var result int
+// Define our own Generic with interface
+type addable interface {
+	int | float64 | string
+}
+
+func addVal[V addable](s []V) V {
+	var result V
 	for _, v := range s {
 		result += v
 	}
