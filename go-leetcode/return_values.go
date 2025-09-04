89978b16a989d63601b85e4e10aa3f66dbeb17c6
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2025-09-04 01:05:37 -0700

    Return values in Go

--- go-leetcode/return_values.go
+++ go-leetcode/return_values.go
@@ -9,9 +9,18 @@ func divide(l, r int) (int, bool) {
 	return l / r, true
 }
 
+// Method on custom type
+type myInt int
+
+func (i myInt) isEven() bool { // Method with receiver of type myInt
+	return i%2 == 0
+}
+
 func main() {
 	result, ok := divide(10, 2)
 	if ok {
 		fmt.Println("Result:", result)
 	}
+	num := myInt(4)
+	fmt.Println("Is even:", num.isEven())
 }
