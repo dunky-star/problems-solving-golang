bab6e446d27bb43af6ef249ca48bf96727a832c0
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2025-10-03 02:29:55 -0700

    feat(sync Once): Optimizing DB Connection call

--- rdbms-go/main.go
+++ rdbms-go/main.go
@@ -4,40 +4,49 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
-	// DSN string
-	dsn := "postgres://appuser:S3cure!@localhost:26257/bank?sslmode=require"
+	Run()
+}
 
-	// Configure pool with sensible defaults
-	conn, err := pgxpool.ParseConfig(dsn)
-	if err != nil {
-		log.Fatal("Unable to parse config:", err)
-	}
+var o sync.Once
 
-	// Optional: tweak pool settings (best practice for prod)
-	conn.MaxConns = 4                         // max number of connections
-	conn.MinConns = 2                         // keep warm connections
-	conn.MaxConnLifetime = time.Hour          // recycle connections hourly
-	conn.HealthCheckPeriod = 30 * time.Second // ping connections
+func Run() {
+	o.Do(func() {
+		// DSN string
+		dsn := "postgres://appuser:S3cure!@localhost:26257/bank?sslmode=require"
 
-	// Connect pool
-	pool, err := pgxpool.NewWithConfig(context.Background(), conn)
-	if err != nil {
-		log.Fatal("Unable to create pool:", err)
-	}
-	defer pool.Close()
+		// Configure pool with sensible defaults
+		conn, err := pgxpool.ParseConfig(dsn)
+		if err != nil {
+			log.Fatal("Unable to parse config:", err)
+		}
 
-	var now time.Time
-	err = pool.QueryRow(context.Background(), "SELECT now()").Scan(&now)
-	if err != nil {
-		log.Fatal("Query failed: ", err)
-	}
+		// Optional: tweak pool settings (best practice for prod)
+		conn.MaxConns = 4                         // max number of connections
+		conn.MinConns = 2                         // keep warm connections
+		conn.MaxConnLifetime = time.Hour          // recycle connections hourly
+		conn.HealthCheckPeriod = 30 * time.Second // ping connections
 
-	fmt.Println("Connected, server time is:", now)
+		// Connect pool
+		pool, err := pgxpool.NewWithConfig(context.Background(), conn)
+		if err != nil {
+			log.Fatal("Unable to create pool:", err)
+		}
+		defer pool.Close()
+
+		var now time.Time
+		err = pool.QueryRow(context.Background(), "SELECT now()").Scan(&now)
+		if err != nil {
+			log.Fatal("Query failed: ", err)
+		}
+
+		fmt.Println("Connected, server time is:", now)
+	})
 
 }
