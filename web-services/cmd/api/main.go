5f060e1fb1071eafbd33ed6c35bc58a8aa9fe299
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2025-10-05 13:18:51 -0700

    feat(Web services): Working with net/http package

--- web-services/cmd/api/main.go
+++ web-services/cmd/api/main.go
@@ -12,9 +12,12 @@ For example, it does not have built-in support for routing, middleware, or templ
 	-Authentication
 	-Panic recovery
 	-CORS / rate limiting
+
+	*** Routing is limited to exact path matching and http.ServeMux does not support path parameters (e.g. /users/{id})
 */
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -22,19 +25,37 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/greeting", greetingHandler)
+	mux.HandleFunc("/v1/greeting", greetingHandler)
+	mux.HandleFunc("/v1/healthcheck", healthCheckHandler)
 
-	port := ":8080"
-	fmt.Printf("Server is running on http://localhost%s\n", port)
+	PORT := ":4000"
+	fmt.Printf("Server is running on http://localhost%s\n", PORT)
 
 	// Start the server and log any error if it fails
-	err := http.ListenAndServe(port, mux)
+	err := http.ListenAndServe(PORT, mux)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
+var startTime = time.Now()
+
 func greetingHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprintf(w, "Labor Omnia Vincit — %s\n", time.Now().Format(time.RFC1123))
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	// Calculate uptime dynamically
+	uptime := time.Since(startTime).Truncate(time.Second)
+	status := map[string]interface{}{
+		"status":    "available",
+		"uptime":    uptime.String(),
+		"timestamp": time.Now().Format(time.RFC3339),
+	}
+
+	if err := json.NewEncoder(w).Encode(status); err != nil {
+		http.Error(w, fmt.Sprintf(`{"error":"%s"}`, err.Error()), http.StatusInternalServerError)
+	}
 }
---
+++ web-services/go.mod
@@ -0,0 +1,3 @@
+module readinglist.dunky.io
+
+go 1.25.0
